7a3b159454e47767590274ebcde78de9b778cf20
Trevor Thomas <thomastrevor1@gmail.com>
2020-06-24 20:40:15 +0000

    Added 5 queries

---
+++ 1_queries/5_reservations_per_user.sql
@@ -0,0 +1,31 @@
+SELECT properties.*, reservations.*, avg(rating) as average_rating
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id 
+WHERE reservations.guest_id = 1
+AND reservations.end_date < now()::date
+GROUP BY properties.id, reservations.id
+ORDER BY reservations.start_date
+LIMIT 10;
+
+
+
+
+
+
+
+
+
+-- My solution that still worked
+-- SELECT reservations.*, properties.*, avg(property_reviews.rating) as average_rating
+-- FROM property_reviews
+-- JOIN users ON users.id = guest_id
+-- JOIN reservations ON reservations.id = reservation_id
+-- JOIN properties ON properties.id = property_reviews.property_id
+-- WHERE reservations.guest_id = 1 
+-- AND reservations.end_date < now()::date
+-- GROUP BY properties.id, reservations.id
+-- ORDER BY reservations.start_date; 
+
+
+
